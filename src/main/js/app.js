6c1915b40d206d317365fb84f416d16289348f5b
Кристина <seriouskris@mail.ru>
2018-06-17 22:21:58 +0200

    Display list of messages.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -2,11 +2,62 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-//const client = require('./client');
+
+const rest = require('rest');
+const mime = require('rest/interceptor/mime');
+const entity = require('rest/interceptor/entity');
+const client = rest
+              .chain(mime, { mime: 'application/json' })
+              .chain(entity);
+
+class Message extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+	    var message = this.props.data;
+	    return (
+	        <div className="message">
+                <div className="message-author">{message.userName}</div>
+                <div className="message-text">{message.text}</div>
+                <div className="message-time">{message.date}</div>
+            </div>
+        );
+	}
+}
+
+class MessageList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { messages: [] };
+    }
+
+    componentDidMount() {
+        rest({ method: 'GET', path: '/messages' }).done(response => {
+            console.log("got msgs", response.entity);
+            var messages = JSON.parse(response.entity).map(m => {
+                m.date = new Date(m.time);
+                return m;
+            });
+            this.setState({ messages: messages });
+        });
+    }
+
+    render() {
+        if (this.state.messages.length > 0) {
+            var messages = this.state.messages.map(msg => <Message key={msg.date.getTime()} data={msg}/>);
+            return (<div className="message-list">{messages}</div>);
+        }
+        return (<div>No messages yet.</div>);
+    }
+}
 
 class App extends React.Component {
     render() {
-        return (<div>It works!</div>);
+        return (<div className="message-app">
+            <MessageList/>
+        </div>);
     }
 }
 
