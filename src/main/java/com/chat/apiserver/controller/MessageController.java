4c6e86fb9cf21ed0e4f71388010a4f381ba219ef
Кристина <seriouskris@mail.ru>
2018-06-20 09:19:56 +0200

    Have own sse emitter for each connection.

--- src/main/java/com/chat/apiserver/controller/MessageController.java
+++ src/main/java/com/chat/apiserver/controller/MessageController.java
@@ -11,12 +11,13 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 import com.chat.apiserver.domain.MessageService;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
 
-    SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+    ArrayList<SseEmitter> sseEmitterList = new ArrayList<SseEmitter>();
 
     @Autowired
     private MessageService messagesService;
@@ -31,15 +32,20 @@ public class MessageController {
     @RequestMapping(method = POST, produces = "application/json")
     public void addMessage(@RequestBody Message message) {
         messagesService.addMessage(message);
-        try {
-            sseEmitter.send(message);
-        } catch (java.io.IOException ex) {
-            System.out.println("Can't send a message through sse: " +  ex.toString());
+        for (int i = sseEmitterList.size()-1; i >= 0; i--) {
+            try {
+                sseEmitterList.get(i).send(message);
+            } catch (java.io.IOException ex) {
+                sseEmitterList.remove(sseEmitterList.get(i));
+                System.out.println("Can't send a message through sse: " + ex.toString());
+            }
         }
     }
 
     @RequestMapping(value="/newMessages", method = GET, produces="text/event-stream")
     SseEmitter newMessages() {
+        SseEmitter sseEmitter = new SseEmitter(Long.MAX_VALUE);
+        sseEmitterList.add(sseEmitter);
         return sseEmitter;
     }
 }
